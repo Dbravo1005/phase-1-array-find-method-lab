15ebd2575fbfce1a42d020cc054ded196bc91ff7
Damian Bravo <Dbravo1005@gmail.com>
2023-05-16 21:31:02 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+const record = [
+    { year: "2002", result: 'W'},
+    { year: "2003", result: 'W'},
+    { year: "2005", result: 'N/A'},
+    { year: "2007", result: 'W'},
+    { year: "2009", result: 'L'},
+    { year: "2011", result: 'L'},
+    { year: "2015", result: 'W'},
+];
+
+function superbowlWin(collection){
+    for(const record of collection){
+        if (record.result === 'W'){
+            return record.year;
+        }
+    }
+    return undefined;
+};
+
+record.find(superbowlWin);
